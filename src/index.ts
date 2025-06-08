53af4682f476f06784f8140f55d55d7e2273334e
Steel64 <updown1.jp@gmail.com>
2025-06-08 12:13:58 -0500

    update documentacion endpoint-2do_endpoint

--- src/index.ts
+++ src/index.ts
@@ -11,6 +11,14 @@ app.get("/", (req : Request , resp : Response) => {
     resp.send("Endpoint raiz")
 })
 
+app.get("/todos", (req : Request , resp : Response)=> {
+    const todos = [
+        {"id" : 1, "descripcion" : "Tomar agua"},
+        {"id" : 2, "descripcion" : "Estudiar"}
+    ]
+    resp.json(todos)
+})
+
 //Aplicacion se ejecute, escucha al puerto
 app.listen(PORT, () => {
     console.log(`se inicio en servidor en puerto ${PORT}`)
